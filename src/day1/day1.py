781e175f005f8b1432f8aadb209597e33eb08602
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2025-12-01 19:11:25 -0600

    hi

--- src/day1/day1.py
+++ src/day1/day1.py
@@ -2,28 +2,29 @@ def password(filename):
     with open(filename, "r") as file:
         lines = file.readlines()
 
-    list_of_passwords = [line.replace("\n", "").strip() for line in lines]
-
+    list_of_passwords = [line.replace("\n", "") for line in lines]
+    
     starting = 50
     
-
     number_of_zeros = 0
     for index, combo in enumerate(list_of_passwords):
-        
-        if combo.count("R") == 1:
+        if combo[0].upper() == "R":
+            
             number = int(combo.replace("R", ""))
+            
             for i in range(number):
                 starting += 1
-                
                 if starting == 100:
                     starting = 0
-
-            if starting == 0 and list_of_passwords[index+1].count("L") == 1:
-                number_of_zeros += 1
             
-        elif combo.count("L") == 1:
+            if starting == 0:
+                number_of_zeros += 1
+        elif combo[0].upper() == "L":
+            
             number = int(combo.replace("L", ""))
+            
             for i in range(number):
+                
                 starting = starting - 1
                 
                 if starting == -1:
@@ -31,7 +32,6 @@ def password(filename):
         
             if starting == 0:
                 number_of_zeros += 1
-
     return number_of_zeros
 print(password("src/day1/day1.txt"))
 print(password("src/day1/test_day1.txt"))
