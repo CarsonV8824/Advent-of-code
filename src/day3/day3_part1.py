200856df9488e03678f4306b4e65ac75e25e3f2e
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2025-12-04 08:13:56 -0600

    Update day3_part1.py

--- src/day3/day3_part1.py
+++ src/day3/day3_part1.py
@@ -3,6 +3,20 @@ def staircase(filename):
         file_lines = file.readlines()
         
     file_lines = [line.replace("\n","") for line in file_lines]
+    file_lines = [int(line) for line in file_lines]
+    
+    for big_num in file_lines:
+        first_num = 0
+        first_num_index = 0
+        
+        for index, num in enumerate(big_num):
+            if num > first_num:
+                first_num = num
+                first_num_index = index
+        
+        if first_num_index == (len(str(big_num)) - 1):
+            for i in range(0, len(str(big_num)) - 2):
+                pass
     
 if __name__ == "__main__":
     print(staircase("src/day3/test_day3.txt"))
